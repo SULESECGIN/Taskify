936cc0694885377764214b1b2399243fa6a13762
Şule Seçgin <yazar4573@gmail.com>
2024-04-05 13:43:46 +0300

    Taskifyİkinciİlerleme
    Login kısmını tamamlıyorum,hata aldım.

---
+++ taskify/src/components/auth/Login.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function login() {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  )
+}
