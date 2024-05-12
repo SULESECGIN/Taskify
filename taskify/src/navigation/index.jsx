e4cb587fdaeccb463847afc94d7c7663f12c88df
Şule Seçgin <yazar4573@gmail.com>
2024-05-12 22:57:44 +0300

    6.hafta
    Proje Redux'a taşındı ve loader ekranı yapıldı.

--- taskify/src/navigation/index.jsx
+++ taskify/src/navigation/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import UserStack from './UserStack';
 import AuthStack from './AuthStack';
+import Loader from '../components/shared/Loader';
+
+
+
 
 export default function NavigationStack() {
+  const { loader } = useSelector((state) => state.general);
   const isLogin = false;
+  //const loader = true;
   return (
     <>
     {
       isLogin ? <UserStack /> : <AuthStack />
+    }
+    {
+      loader && <Loader Loader={loader} />
     }
     </>
   )
